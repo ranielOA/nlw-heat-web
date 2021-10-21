5036551c37216d9328f0b39ef50936903540ef2f
ranielOA <r.a_tw@hotmail.com>
2021-10-20 23:21:09 -0300

    created provider

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from 'react';
-// import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
-// import { useAuth } from '../../hooks/useAuth';
-// import { api } from '../../services/api';
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
+import { useAuth } from '../../hooks/useAuth';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
@@ -9,7 +9,7 @@ export function SendMessageForm() {
   const [message, setMessage] = useState('');
   const [isSendingMessage, setIsSendingMessage] = useState(false);
 
-  // const { user, signOut } = useAuth()
+  const { user, signOut } = useAuth();
 
   async function handleSendMessage(event: FormEvent) {
     event.preventDefault();
@@ -21,9 +21,9 @@ export function SendMessageForm() {
     setIsSendingMessage(true);
 
     try {
-      // await api.post('messages', {
-      //   message,
-      // })
+      await api.post('messages', {
+        message,
+      });
 
       setMessage('');
     } finally {
@@ -33,18 +33,18 @@ export function SendMessageForm() {
 
   return (
     <div className={styles.sendMessageFormWrapper}>
-      {/* <button onClick={signOut} className={styles.signOutButton}>
+      <button onClick={signOut} className={styles.signOutButton}>
         <VscSignOut size={32} />
-      </button> */}
+      </button>
 
       <header className={styles.signedUserInformation}>
         <div className={styles.userImage}>
-          {/* <img src={user?.avatar_url} alt={user?.name} /> */}
+          <img src={user?.avatar_url} alt={user?.name} />
         </div>
-        {/* <strong className={styles.userName}>{user?.name}</strong> */}
+        <strong className={styles.userName}>{user?.name}</strong>
         <span className={styles.userGithub}>
-          {/* <VscGithubInverted size={16} />
-          {user?.login} */}
+          <VscGithubInverted size={16} />
+          {user?.login}
         </span>
       </header>
 
